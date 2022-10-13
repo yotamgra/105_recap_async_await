ae32d4f151c008eacc3980b032927637a868ec24
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-10-13 17:38:06 +0200

    fetch completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const fetchProducts = async () => {
+  const response = await fetch("https://dummyjson.com/products");
+  const { products } = await response.json();
+
+  console.log(products);
+};
+
+fetchProducts();
